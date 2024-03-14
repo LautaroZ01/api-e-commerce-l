7400b6c026b2ab7134d3699f01462ad2ba0a9135
Lautaro Zuleta <lautarozule80@gmail.com>
2024-03-14 16:49:57 -0300

    Imagenes arreglado

--- src/Routers/RProduct.js
+++ src/Routers/RProduct.js
@@ -1,18 +1,6 @@
 import { Router } from "express";
-import {getCategorias, getOneProduct, getProducts, media, upload} from "../Controllers/CProduct.js"
-import multer from "multer";
-import { check } from "../Middlewares/auth.js";
+import {getCategorias, getOneProduct, getProducts} from "../Controllers/CProduct.js"
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./Uploads/Products/")
-    },
-    filename: (req, file, cb) => {
-        cb(null, "product-" + Date.now() + "-" + file.originalname);
-    }
-});
-
-const uploads = multer({ storage });
 
 const routerP = Router();
 
@@ -20,8 +8,6 @@ const routerP = Router();
 routerP.get('/list', getCategorias);
 routerP.get('/products', getProducts);
 routerP.get('/oneproduct/:id', getOneProduct);
-routerP.post('/upload/:id', [check, uploads.single("photo")], upload);
-routerP.get("/media/:file", media);
 
 
 export {routerP};
